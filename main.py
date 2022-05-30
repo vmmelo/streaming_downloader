07887b61ebdf02d565ec350c91e297eb576144e4
Victor Miranda <vmmelo1@gmail.com>
2022-05-30 16:17:04 -0300

    console colors

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from pytube import YouTube
 import sys
+import os
+
+DOWNLOAD_FOLDER = 'yt_downloader'
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def download_yt_video(video_url):
     try:
-        print('validating video url')
         yt = YouTube(video_url)
-        print(yt.title)
+        print_cli(f'downloading video: {yt.title}')
         if yt.length > 600:
             raise Exception('The video needs to have less than 10 minutes length')
-        yt\
-            .streams\
-            .filter(progressive=True, file_extension='mp4')\
-            .order_by('resolution')\
-            .desc()\
-            .first()\
-            .download(output_path=f'outputs/')
+        yt \
+            .streams \
+            .filter(progressive=True, file_extension='mp4') \
+            .order_by('resolution') \
+            .desc() \
+            .first() \
+            .download(output_path=DOWNLOAD_FOLDER)
     except Exception as e:
-        print(str(e))
+        print_cli(e, 'error')
     else:
-        print("Video downloaded successfully")
+        print_cli(f'Video downloaded successfully on {os.path.join(os.getcwd(), DOWNLOAD_FOLDER)}', 'success')
+
+
+def print_cli(text, message_type=''):
+    r = 0
+    g = 0
+    b = 0
+    if message_type == 'error':
+        r = 255
+    elif message_type == 'success':
+        g = 255
+    return print("\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text))
 
 
 if __name__ == '__main__':
-    print(sys.argv, len(sys.argv))
     if len(sys.argv) < 2:
-        print('please specify the video_url argument')
-    download_yt_video(sys.argv[1])
+        cls()
+        print_cli('please, specify the video_url argument', 'error')
+    else:
+        download_yt_video(sys.argv[1])
